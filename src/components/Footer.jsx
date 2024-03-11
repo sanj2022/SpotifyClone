13d48d2b3fc264af32e0e68ed1457c616a54e18b
shokeensanjana <sashokeen@expediagroup.com>
2024-03-11 15:59:47 +0530

    Added player Controls

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import CurrentTrack from './CurrentTrack'
+import PlayerControls from './PlayerControls'
+import Volume from './Volume'
+
+export default function Footer() {
+  return (
+    <Conatiner>
+        <CurrentTrack/>
+        <PlayerControls/>
+        <Volume/>
+        </Conatiner>
+  )
+}
+
+const Conatiner = styled.div`
+background-color:#181818;
+height:100%;
+width: 100%;
+border-top:1px solid #282828;
+display:grid;
+grid-template-columns:1fr 2fr 3fr;
+align-items: center;
+padding: 0 1rem;
+
+`
